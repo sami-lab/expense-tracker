37757f368c8893b61dd94525cd6575ca457482fa
Sami <s.m.sami125@gmail.com>
2021-10-31 23:37:33 +0500

    api

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let baseURL = 'http://localhost:5000/api/v1';
-baseURL = 'https://authentication-testing.herokuapp.com/api';
+baseURL = 'https://authentication-testing.herokuapp.com/api/v1';
 const instance = axios.create({
   baseURL,
 });
